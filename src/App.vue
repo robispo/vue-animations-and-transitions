9830bb1a2d25ce01cb36b7fa1592e061f2245407
Rabel Obispo <rabelobispo@hotmail.com>
2020-01-23 22:37:19 -0500

    transition and animation together

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
         <transition name="fade">
           <div class="alert alert-info" v-if="show">This is some info.</div>
         </transition>
-        <transition name="slide">
+        <transition name="slide" type="animation">
           <div class="alert alert-info" v-if="show">This is some info.</div>
         </transition>
       </div>
@@ -46,14 +46,18 @@
   }
 
   .slide-enter {
+    opacity: 0;
   }
   .slide-enter-active {
     animation: slide-in 1s ease-out forwards;
+    transition: opacity 0.5s;
   }
   .slide-leave {
   }
   .slide-leave-active {
     animation: slide-out 1s ease-out forwards;
+    transition: opacity 3s;
+    opacity: 0;
   }
 
   @keyframes slide-in {
